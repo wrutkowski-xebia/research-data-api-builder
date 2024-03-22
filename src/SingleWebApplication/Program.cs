7e8db0edfd899a972ed9b94b8cd6fbeb57c3d9ae
Waldemar Rutkowski <waldemar.rutkowski@xebia.com>
2024-03-22 15:00:14 +0100

    blazor authentication

--- src/SingleWebApplication/Program.cs
+++ src/SingleWebApplication/Program.cs
@@ -8,4 +8,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMsalAuthentication(options =>
+{    
+    options.ProviderOptions.LoginMode = "redirect";
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+});
+
 await builder.Build().RunAsync();
