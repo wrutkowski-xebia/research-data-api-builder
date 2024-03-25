9482f8703964ddbe4fb8ca1c4497c7cc86126c3f
Waldemar Rutkowski <waldemar.rutkowski@xebia.com>
2024-03-25 11:19:22 +0100

    test api

---
+++ src/SingleWebApplication.API/Function1.cs
@@ -0,0 +1,23 @@
+using System;
+using Azure.Storage.Queues.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace SingleWebApplication.API
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(Function1))]
+        public void Run([QueueTrigger("myqueue-items", Connection = "")] QueueMessage message)
+        {
+            _logger.LogInformation($"C# Queue trigger function processed: {message.MessageText}");
+        }
+    }
+}
