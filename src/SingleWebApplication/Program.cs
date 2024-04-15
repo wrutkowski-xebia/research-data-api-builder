d139cc6f6a615f7e6c32cf8672228664aa4d2755
Waldemar Rutkowski <waldemar.rutkowski@xebia.com>
2024-04-15 11:32:35 +0200

    auth

--- src/SingleWebApplication/Program.cs
+++ src/SingleWebApplication/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddMsalAuthentication(options =>
 {    
     options.ProviderOptions.LoginMode = "redirect";
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.UserOptions.RoleClaim = "Sample.Role";
 });
 
 builder.Services.AddHttpClients(builder.Configuration);
